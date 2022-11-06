3491a611c71713e6f57de3d4e91052c37aeb15ee
crisandryj <crisandryj@gmail.com>
2022-11-06 18:51:14 -0500

    update

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
   def index
-    client = Flickr::Client.new( '3050ed837068a0a24f259c3b17e08828' )
+    client = Flickr::Client.new('3050ed837068a0a24f259c3b17e08828')
   end
 end
